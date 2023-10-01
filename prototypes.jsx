b680fab1a382eb44aab7486d45111dd1a96d6ef5
JK <jayedulk@gmail.com>
2023-09-30 21:07:48 -0400

    Path fixed

--- prototypes.jsx
+++ prototypes.jsx
@@ -1,4 +1,4 @@
-import style_library from '../styles/index.module.scss';
+import style_library from './styles/index.module.scss';
 import icons from './static/icons/crewhrm/style.module.scss';
 import { is_production } from './helpers.jsx';
 
