2451fa0036b29f66f276b79257ac2b6b39bcc5f0
JK <jayedulk@gmail.com>
2023-10-03 20:52:52 -0400

    External render update

--- render-external.jsx
+++ render-external.jsx
@@ -4,9 +4,13 @@ import { isFirstLetterCapitalized } from "./helpers.jsx";
 export function RenderExternal({component: Comp, payload={}}) {
 
 	const reff = useRef();
-	const is_component = isFirstLetterCapitalized(Comp.toString().replace('function ', ''));
+
+	const func_string = Comp.toString();
+	const is_component = func_string.indexOf('function ')===0 && isFirstLetterCapitalized(func_string.replace('function ', ''));
 
 	useEffect(()=>{
+
+		console.log(is_component, func_string);
 
 		if ( ! is_component && reff && reff.current) {
 			Comp(reff.current, payload);
