df08ee190a180fe54f775922600bd0bc2b223869
JK <jayedulk@gmail.com>
2023-10-09 17:01:26 -0400

    External renderer bug fix

--- render-external.jsx
+++ render-external.jsx
@@ -1,20 +1,9 @@
 import React, { useEffect, useRef } from "react";
 
-function isFunctionNameCapitalized(func) {
-  if (typeof func === 'function') {
-    const functionName = func.name;
-    if (functionName.length > 0) {
-      const firstChar = functionName.charAt(0);
-      return /^[A-Z]/.test(firstChar); // Check if it starts with a capital letter, so it is a React component.
-    }
-  }
-  return false;
-}
-
 export function RenderExternal({component: Comp, payload={}}) {
 
 	const reff = useRef();
-	const is_component = isFunctionNameCapitalized(Comp);
+	const is_component = Comp.toString().indexOf('createRoot')===-1;
 
 	useEffect(()=>{
 		if ( ! is_component && reff && reff.current) {
