11e7cac4b0f2c7ea251faa6f349ae977cbdf2c47
JK <jayedulk@gmail.com>
2023-10-09 14:24:48 -0400

    Address string undefined fix

--- helpers.jsx
+++ helpers.jsx
@@ -148,7 +148,7 @@ export function scrollLock(lock) {
 }
 
 export function getAddress({ street_address, city, province, zip_code, country_code }) {
-	return [street_address, city, province + ' ' + zip_code, countries_object[country_code]]
+	return [street_address, city, (province || '') + ' ' + (zip_code || ''), countries_object[country_code]]
 		.map((a) => (a || '').trim())
 		.filter((a) => a)
 		.join(', ');
