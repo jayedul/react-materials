81fe06e5adbce20ab514214a2e431614502be82f
JK <jayedulk@gmail.com>
2023-10-12 21:06:27 -0400

    Optimization

--- render-external.jsx
+++ render-external.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useRef } from "react";
 
 export function RenderExternal({component: Comp, payload={}}) {
+	if ( ! Comp ) {
+		return null;
+	}
 
 	const reff = useRef();
 	const is_component = Comp.toString().indexOf('createRoot')===-1;
