4381db5c2b9d5a330372ba18563eb485f4004501
JK <jayedulk@gmail.com>
2023-10-14 02:30:53 -0400

    Render external bug fix

--- render-external.jsx
+++ render-external.jsx
@@ -6,15 +6,10 @@ export function RenderExternal({component: Comp, payload={}}) {
 	}
 
 	const reff = useRef();
-	const is_component = Comp.toString().indexOf('createRoot')===-1;
-
-	console.log( 'Log1 - ', Comp.toString(), is_component );
+	const is_component = Comp.length!==2; // 2 means function callback, not component.
 
 	useEffect(()=>{
 		if ( ! is_component && reff && reff.current) {
-
-			console.log('Log 2 - ', reff.current, payload);
-
 			Comp(reff.current, payload);
 		}
 
