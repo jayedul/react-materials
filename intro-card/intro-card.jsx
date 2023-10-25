d87459a802635f260a16d0a8019c08a5b27a19a9
JK <jayedulk@gmail.com>
2023-10-24 20:57:44 -0400

    Dashboard intro card responsiveness enhanced

--- intro-card/intro-card.jsx
+++ intro-card/intro-card.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 import style from './intro.module.scss';
 
@@ -17,18 +17,35 @@ export function IntroCard(props) {
 
     const is_horizontal = orientation == 'horizontal';
     const image_url = images[image];
+	const ref_wrapper = useRef();
+	const [imageState, setImageState] = useState();
+
+	const setImage=()=>{
+		setImageState(ref_wrapper.current.offsetWidth>500);
+	}
+
+	useEffect(()=>{
+		if ( ref_wrapper?.current && is_horizontal ) {
+			setImage();
+			window.addEventListener('resize', setImage);
+			return ()=>window.removeEventListener('resize', setImage);
+		}
+	}, [ref_wrapper, is_horizontal]);
 
     return (
         <div
+			ref={ref_wrapper}
             data-crew={'intro-card-' + orientation}
             className={
                 `intro orientation-${orientation}`.classNames(style) +
                 'bg-color-white border-radius-5'.classNames() +
                 className
             }
-            style={{ backgroundImage: 'url(' + image_url + ')', ...cssStyle }}
+            style={{ backgroundImage: imageState ? 'url(' + image_url + ')' : '', ...cssStyle }}
         >
-            <div className={'content'.classNames(style)}>{props.children}</div>
+            <div className={'content'.classNames(style)} style={imageState ? {width: 'calc(100% - 230px)'} : {}}>
+				{props.children}
+			</div>
             <div className={'image'.classNames(style)}>
                 {(!is_horizontal && <img src={image_url} />) || null}
             </div>
