208a8a350b5940f9f3ccd650e285393bf9c523a6
JK <jayedulk@gmail.com>
2023-12-23 15:03:20 -0500

    External component render enhanced

--- render-external.jsx
+++ render-external.jsx
@@ -1,21 +1,38 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { createRoot } from "react-dom/client";
 
 import {ErrorBoundary} from './error-boundary.jsx';
+import {getRandomString} from './helpers.jsx';
 
 export function RenderExternal({component: Comp, payload={}}) {
 	if ( ! Comp ) {
 		return null;
 	}
 
+	const [sessionState, setSessionState] = useState(getRandomString());
 	const reff = useRef();
-	const is_component = Comp.length!==2; // 2 means function callback, not component.
+	const is_internal = Comp.length!==2; // 2 means it is from external script.
 
 	useEffect(()=>{
-		if ( ! is_component && reff && reff.current) {
-			Comp(reff.current, payload);
+		// If not internal componenet, call the function, so it can render their contents. 
+		if ( ! is_internal && reff && reff.current) {
+			Comp(reff.current, {session: sessionState, payload});
 		}
-
 	}, [Comp, reff.current, payload]);
 
-	return is_component ? <ErrorBoundary><Comp {...payload}/></ErrorBoundary> : <div ref={reff}></div>
+	// If internal component, then simply embed as usual. External component can only be called as function through useEffect hook.
+	return is_internal ? <ErrorBoundary><Comp {...payload}/></ErrorBoundary> : <div ref={reff}></div>
+}
+
+const render_states={};
+export function mountExternal( id, el, session, component ) {
+
+	if ( render_states[id]?.session !== session ) {
+		render_states[id] = {
+			session,
+			root: createRoot(el)
+		}
+	}
+
+	render_states[id].root.render(component);
 }
