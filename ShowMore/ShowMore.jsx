95e696221baea0ad716d83232252ed954e588b35
JK <jayedulk@gmail.com>
2024-01-31 17:38:43 -0500

    Employee Assets copied into

---
+++ ShowMore/ShowMore.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+// import { __ } from 'crewhrm-materials/helpers.jsx';
+import IndexCss from './index.module.scss';
+
+// eslint-disable-next-line react/prop-types
+export default function ShowMore({ text = 'Show additional option', expand = false, reverse = false, image = false }) {
+	return (
+		<div
+			className={
+				`d-flex align-items-center column-gap-15 cursor-pointer
+                ${reverse == true ? 'flex-direction-row-reverse' : ''}
+                `.classNames() + 'show-more'.classNames(IndexCss)
+			}
+		>
+			{image && <img src={image} alt="" />}
+			{!image && (
+				<div
+					className={
+						''.classNames() + `show-more-icon ${expand == true ? 'expand' : ''} `.classNames(IndexCss)
+					}
+				>
+					<span></span>
+					<span></span>
+				</div>
+			)}
+			<div className={'font-size-15 font-weight-500 line-height-24'.classNames()}>{text}</div>
+		</div>
+	);
+}
---
+++ StepProgress/StepProgress.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { __ } from 'crewhrm-materials/helpers.jsx';
+import IndexCss from './index.module.scss';
+
+// eslint-disable-next-line react/prop-types
+export default function StepProgress({ completedStep = 0, currentStep, totalStep, reverse = false }) {
+	return (
+		<div className={'d-flex align-items-center column-gap-10'.classNames()}>
+			<div
+				className={`flex-1
+                ${reverse == true ? 'flex-direction-row-reverse' : ''}
+                d-flex align-items-center justify-content-end column-gap-10 color-text font-size-15 font-weight-500`.classNames()}
+			>
+				<div className={'d-flex justify-content-end'.classNames()}>
+					{currentStep}/{totalStep} {__('completed')}
+				</div>
+				<div className={'complete-progressbar'.classNames(IndexCss) + ''.classNames()}>
+					<span className={'complete-progressbar-bar'.classNames(IndexCss) + 'margin-left-10'.classNames()}>
+						<span
+							style={{ width: `${(60 / 4) * (completedStep + 1)}px` }}
+							className={'upgradeable-progressbar-bar'.classNames(IndexCss) + ''.classNames()}
+						></span>
+					</span>
+				</div>
+			</div>
+		</div>
+	);
+}
