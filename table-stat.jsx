f5d054660342fa8c8e06a4f59eeb6fdcf2697422
JK <jayedulk@gmail.com>
2024-03-12 17:34:29 -0400

    UI enhancements

--- table-stat.jsx
+++ table-stat.jsx
@@ -3,18 +3,23 @@ import React from "react";
 import {LoadingIcon} from './loading-icon/loading-icon.jsx';
 import { __ } from "./helpers.jsx";
 
-export function TableStat({empty, loading, message}) {
-	return (!empty && !loading) ? null : <tr>
+export function TableStat({empty, loading, message, children}) {
+	return (!empty && !loading) ? null : <tr className={'empty-state'.classNames()}>
 		<td colSpan="100%">
-			<div className={'text-align-center padding-vertical-20'.classNames()}>
-				{
-					loading ? 
-						<LoadingIcon show={loading} center={true}/> : 
+			{
+				loading ? 
+				<div className={'padding-vertical-20'.classNames()}>
+					<LoadingIcon show={loading} center={true}/>
+				</div>
+				:
+				(
+					children || <div className={'text-align-center padding-vertical-20'.classNames()}>
 						<strong className={'font-weight-500'.classNames()}>
 							{message || __('No result!')}
 						</strong>
-				}
-			</div>
+					</div>
+				)
+			}
 		</td>
 	</tr>
 }
--- text-field/text-field.jsx
+++ text-field/text-field.jsx
@@ -211,6 +211,7 @@ export function TextField(props) {
             }
 			style={{
 				height: type==='textarea' ? '100px' : undefined,
+				margin: 0,
 				...cssStyle
 			}}
         >
