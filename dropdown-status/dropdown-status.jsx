be8307067ed468250cca4553342e63280dc532ca
JK <jayedulk@gmail.com>
2024-05-05 00:06:50 +0600

    Status dropdown added, audio opacity update

---
+++ dropdown-status/dropdown-status.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import style from './style.module.scss';
+
+const color_map = {
+	pending: 'warning',
+	publish: 'success',
+	published: 'success',
+	rejected: 'error',
+	banned: 'error',
+}
+
+export function DropDownStatus({options, value, onChange, disabled}) {
+
+	return <div className={`select-wrapper ${color_map[value] || 'default'} ${disabled ? 'disabled' : ''}`.classNames(style)}>
+		<select 
+			onChange={e=>onChange(e.currentTarget.value)} 
+			value={value}
+			disabled={disabled}
+		>
+			{
+				options.map(option=>{
+					return <option key={option.id} value={option.id}>
+						{option.label}
+					</option>
+				})
+			}
+		</select>
+		<i className={`${'icon2'.classNames(style)} ${'ch-icon ch-icon-arrow-down'.classNames()}`}></i>
+	</div>
+}
