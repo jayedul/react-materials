787b3d677e9a75bea9d0cfc4b294797bd3db217c
JK <jayedulk@gmail.com>
2024-05-13 15:19:25 +0600

    Loading icon fix

--- loading-icon/loading-icon.jsx
+++ loading-icon/loading-icon.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
 
 import style from './loading.module.scss';
-import { Conditional } from '../conditional.jsx';
 
-export function LoadingIcon({
-    show = true,
-    center = false,
-    className
-}) {
-    return (
-        <Conditional show={show}>
-            <div
-                className={
-                    `${center ? 'd-block text-align-center' : 'd-inline-block'}`.classNames() +
-                    className
-                }
-            >
-                <i className={'ch-icon ch-icon-loading d-inline-block position-relative top-2'.classNames() + 'rotate'.classNames(style)}></i>
-            </div>
-        </Conditional>
-    );
+export function LoadingIcon({ show, center = false, className }) {
+    return !show ? null : <div
+		className={
+			`${center ? 'd-block text-align-center' : 'd-inline-block'}`.classNames() +
+			className
+		}
+	>
+		<i className={'ch-icon ch-icon-loading d-inline-block position-relative top-2'.classNames() + 'rotate'.classNames(style)}></i>
+	</div>
 }
