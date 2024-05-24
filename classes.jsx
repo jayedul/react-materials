d2dc4dc066372fe1c8f515c8dedb77a960ae0827
JK <jayedulk@gmail.com>
2024-05-25 00:40:57 +0600

    Input class export enhance

--- classes.jsx
+++ classes.jsx
@@ -1,4 +1,5 @@
-export const input_class =
-    'bg-color-white padding-vertical-0 padding-horizontal-15 border-radius-10 border-1-5 b-color-tertiary b-color-active-primary width-p-100 d-block height-40 font-size-15 font-weight-400 line-height-25 color-text'.classNames();
-export const input_class_error =
-    'bg-color-white padding-vertical-0 padding-horizontal-15 border-radius-10 border-1-5 b-color-error width-p-100 d-block height-40 font-size-15 font-weight-400 line-height-25 color-text'.classNames();
+export const input_class_raw = 'bg-color-white padding-vertical-0 padding-horizontal-15 border-radius-10 border-1-5 b-color-tertiary b-color-active-primary width-p-100 d-block height-40 font-size-15 font-weight-400 line-height-25 color-text';
+export const input_class     = input_class_raw.classNames();
+
+export const input_class_error_raw = 'bg-color-white padding-vertical-0 padding-horizontal-15 border-radius-10 border-1-5 b-color-error width-p-100 d-block height-40 font-size-15 font-weight-400 line-height-25 color-text';
+export const input_class_error     = input_class_error_raw.classNames();
