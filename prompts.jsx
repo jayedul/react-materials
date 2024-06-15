8620167834891339193ffbfed672123b057956e9
JK <jayedulk@gmail.com>
2024-06-16 01:55:14 +0600

    confirm equivalent

---
+++ prompts.jsx
@@ -0,0 +1,22 @@
+import Swal from 'sweetalert2';
+import { data_pointer } from './helpers';
+
+const {colors={}} = window[data_pointer];
+
+export function confirm(title, text, callback) {
+	Swal.fire({
+		title,
+		text: typeof text=='function' ? undefined : text,
+		icon: "warning",
+		showCancelButton: true,
+		cancelButtonColor: 'gray',
+		confirmButtonColor: colors.material,
+		confirmButtonText: "Yes",
+		reverseButtons: true
+	}).then((result) => {
+		if (result.isConfirmed) {
+			const func = typeof text=='function' ? text : callback;
+			func();
+		}
+	});
+}
