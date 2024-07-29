ccf04c4fc1adfa47b9d02f5b7a79567f53ab02f6
JK <jayedulk@gmail.com>
2024-07-29 12:42:52 +0600

    Clear dist moved into

---
+++ builders/clear-dist.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const path = require('path');
+
+const directory = path.resolve(path.dirname(require.main.filename) + '/dist');
+
+fs.readdir(directory, (err, files) => {
+	
+  if (err) throw err;
+
+  files.forEach(file => {
+    const filePath = path.join(directory, file);
+    fs.stat(filePath, (err, stat) => {
+      if (err) throw err;
+
+      if (stat.isFile()) {
+        fs.unlink(filePath, err => {
+          if (err) throw err;
+        });
+      }
+    });
+  });
+});
