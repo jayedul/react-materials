d5ce21c7e00e78e90e5293d53655a781a75ebeb9
JK <jayedulk@gmail.com>
2024-08-04 00:14:46 +0600

    data_pointer enhanced

--- helpers.jsx
+++ helpers.jsx
@@ -3,8 +3,8 @@ import { countries_object, patterns } from './data.jsx';
 
 // Determine the unique data pointer
 const matches = document.currentScript.src.match(/\/([^/]+)\/wp-content\/(plugins|themes)\/([^/]+)\/.*/);
-const parsedString = `CrewMat_${matches[1]}_${matches[3]}`.toLowerCase();
-export const data_pointer = parsedString.replace(/[^a-zA-Z0-9_]/g, '');
+const parsedString = `CrewMat_${matches[1]}_${matches[3]}`.toLowerCase().replace(/[^a-zA-Z0-9_]/g, '');
+export const data_pointer = parsedString.endsWith('pro') ? parsedString.slice(0, 'pro'.length) : parsedString;
 
 export const currency_symbol = currencySymbol[window[data_pointer].currency_code];
 
