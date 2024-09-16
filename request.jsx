24580a5296177df392987580cc3847060302dbb6
JK <jayedulk@gmail.com>
2024-09-16 16:01:28 +0600

    Wrong var fix

--- request.jsx
+++ request.jsx
@@ -22,7 +22,7 @@ function calculateFormDataSize(formData) {
 			totalSize += value.size;
 
 			if ( value.size > max_filesize ) {
-				return sprintf( __( 'Individual file size can not exceed the limit %s' ), getHighestUnitFromBytes( value.size ) );
+				return sprintf( __( 'Individual file size can not exceed the limit %s' ), getHighestUnitFromBytes( max_filesize ) );
 			}
 
         } else {
@@ -31,7 +31,7 @@ function calculateFormDataSize(formData) {
         }
     }
 
-    return totalSize > max_post_size ? sprintf( __( 'The total upload size can not exceed the limit %s'), getHighestUnitFromBytes(totalSize) ) : true;
+    return totalSize > max_post_size ? sprintf( __( 'The total upload size can not exceed the limit %s'), getHighestUnitFromBytes(max_post_size) ) : true;
 }
 
 /**
